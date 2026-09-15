9a77d7ef09b1f425119f5f8320e49c7e7718fa98
kabyaxo-svg <kabyaxo@gmail.com>
2026-09-15 16:49:39 +0530

    lyso.js

---
+++ lyso.js
@@ -0,0 +1,12 @@
+function getResult(marks, paasMarks = 40){
+    if(marks>=80){
+        return "A";
+    } else if(marks>=60){
+        return "B";
+    } else if(marks>=passMarks){
+        return "C";
+    } else{
+        return "F";
+    }
+}
+console.log(getResult(72));
