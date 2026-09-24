03769fd2be827f2e01ff0c633442fc266e54facb
kabyaxo-svg <kabyaxo@gmail.com>
2026-09-24 15:27:04 +0530

    Updated student program

---
+++ day2.js
@@ -0,0 +1,16 @@
+let studentName = "Kabya";
+const age = 20;
+var course = "CSE";
+const collegeName = "SITM College";
+let marks = 75;
+var birthYear = 2007;
+studentName = "Aybak";
+marks = 85;
+course = "B.Sc. Data Science";
+
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Course:", course);
+console.log("College Name:", collegeName);
+console.log("Marks:", marks);
+console.log("Birth Year:", birthYear);
